a7e01de490af14b4e283efcd0ca3dee68e12bc7d
Tamás Ficsor <ficstamas@gmail.com>
2020-05-19 15:11:43 +0200

    Fixing paths for experiments

--- experiments/default.sh
+++ experiments/default.sh
@@ -3,9 +3,9 @@
 workspace="baseline"
 methods=("hellinger" "bhattacharyya")
 proc=20
-
+cd ../
 for method in ${methods[*]}
 do
-    python ../interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'random' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "accuracy" -save -accuracy;
-    python ../interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'category_center' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "interpret" -save -interpretability;
+    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'random' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "accuracy" -save -accuracy;
+    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'category_center' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "interpret" -save -interpretability;
 done
