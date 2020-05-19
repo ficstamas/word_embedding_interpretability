b3bf7decc6d565c43852e70516350349f77628ce
Tamás Ficsor <ficstamas@gmail.com>
2020-05-19 17:51:31 +0200

    Experiments: Fixing paths to avoid overwriting

--- experiments/default.sh
+++ experiments/default.sh
@@ -6,6 +6,6 @@ proc=20
 cd ../
 for method in ${methods[*]}
 do
-    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'random' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "accuracy" -save -accuracy;
-    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'category_center' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "interpret" -save -interpretability;
+    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'random' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "${method}_accuracy" -save -accuracy;
+    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'category_center' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "${method}_interpret" -save -interpretability;
 done
--- experiments/mcmc.sh
+++ experiments/mcmc.sh
@@ -9,7 +9,7 @@ for method in ${methods[*]}
 do
   for noise in ${noises[*]}
   do
-    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'random' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "accuracy_${noise}" --model 'mcmc' --mcmc_noise "$noise" -save -accuracy;
-    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'category_center' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "interpret_${noise}" --model 'mcmc' --mcmc_noise "$noise" -save -interpretability;
+    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'random' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "${method}_accuracy_${noise}" --model 'mcmc' --mcmc_noise "$noise" -save -accuracy;
+    python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'category_center' --smc_rate 0.4 --seed 0 --processes $proc --distance "$method" --workspace $workspace --name "${method}_interpret_${noise}" --model 'mcmc' --mcmc_noise "$noise" -save -interpretability;
   done
 done
