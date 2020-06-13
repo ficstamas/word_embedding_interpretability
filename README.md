3c3071d5f401e2a42415dba0dbb301c1d43c4d04
Tamás Ficsor <ficstamas@gmail.com>
2020-06-13 15:34:25 +0200

    Readme update and changed the smc_loader in TSD experiment script

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Every dependency can be found in the [requirements.txt](requirements.txt).<br>
 # TSD recreation of results
 Glove can be downloaded [here](http://nlp.stanford.edu/data/glove.6B.zip) and the SemCat dataset is available [here](https://github.com/avaapm/SEMCATdataset2018).
 
-Run [tsd_experiment.sh](experiments/tsd_expriments.sh) after changing the _glove_path_ and _semcat_path_ variables. Furthermore change the _proc_ variable according to your CPU cores (Default: 30), but it is not going to use more than the available number of cores.
+Run [tsd_experiment.sh](experiments/tsd_expriments.sh) after changing the _glove_path_ and _semcat_path_ variables. Furthermore change the _proc_ variable according to your CPU cores (Default: 30), but it is not going to spawn/fork more processes than the available number of physical cores.
 
 # Example
 `python interpretability_cli.py --embedding_path '../shared/embeddings/glove/dense/glove.6B.300d.txt' -dense --lines_to_read 50000 --smc_path '../shared/semcat/Categories/' --smc_method 'category_center' --processes 6 --distance 'hellinger_normal' --workspace 'test' -save -interpretability`
--- experiments/tsd_expriments.sh
+++ experiments/tsd_expriments.sh
@@ -12,9 +12,9 @@ cd ../
 
 for method in ${methods[*]}
 do
-  python interpretability_cli.py --embedding_path "$glove_path" -dense --lines_to_read "$number_of_words" --smc_path "$semcat_path" --smc_loader 'old_semcat' --smc_method 'random' --smc_rate 0.4 --seed 404 --processes $proc --distance "$method" --workspace "${workspace}/${method}" --name "accuracy" -save -accuracy;
+  python interpretability_cli.py --embedding_path "$glove_path" -dense --lines_to_read "$number_of_words" --smc_path "$semcat_path" --smc_method 'random' --smc_rate 0.4 --seed 404 --processes $proc --distance "$method" --workspace "${workspace}/${method}" --name "accuracy" -save -accuracy;
   for dr in ${drop_rate[*]}
   do
-    python interpretability_cli.py --embedding_path "$glove_path" -dense --lines_to_read "$number_of_words" --smc_path "$semcat_path" --smc_loader 'old_semcat' --smc_method 'category_center' --smc_rate "$dr" --seed 404 --processes $proc --distance "$method" --workspace "${workspace}/${method}" --name "${dr}_interpret" -save -interpretability;
+    python interpretability_cli.py --embedding_path "$glove_path" -dense --lines_to_read "$number_of_words" --smc_path "$semcat_path" --smc_method 'category_center' --smc_rate "$dr" --seed 404 --processes $proc --distance "$method" --workspace "${workspace}/${method}" --name "${dr}_interpret" -save -interpretability;
   done
 done
