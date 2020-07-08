1e0c92f6135c6931d6061540d2eb1b1954c3a14d
Tamás Ficsor <ficstamas@gmail.com>
2020-07-08 13:10:03 +0200

    Fast test file updated with new args

--- experiments/fast_test.sh
+++ experiments/fast_test.sh
@@ -5,8 +5,10 @@ methods=("hellinger")
 proc=30
 semcor="data/semcor/semcor.data.xml"
 bert="data/bert/semcor.bert.dense.l24.npy"
+test_weights="data/bert/all.bert.dense.l24.npy"
+test_words="data/semcor/semcor.eval.data.xml"
 cd ../
 for method in ${methods[*]}
 do
-    python interpretability_cli.py --embedding_path "$bert" -dense -numpy --lines_to_read -1 --smc_loader='semcor' --smc_path "$semcor" --processes $proc --distance "$method" --workspace $workspace --name "${method}_dense" -save --accuracy "accuracy@1";
+    python interpretability_cli.py --test_words_weights "$test_weights" --test_words "$test_words" --embedding_path "$bert" -dense -numpy --lines_to_read -1 --smc_loader='semcor' --smc_path "$semcor" --processes $proc --distance "$method" --workspace $workspace --name "${method}_dense" -save --accuracy "accuracy@1";
 done
