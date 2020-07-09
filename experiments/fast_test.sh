c5e86a7fc20c0ba774cbcb16782275141405763c
Tamás Ficsor <ficstamas@gmail.com>
2020-07-09 17:39:21 +0200

    Accuracy abs

--- experiments/fast_test.sh
+++ experiments/fast_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-workspace="/workspace/bert"
+workspace="workspace/bert"
 methods=("hellinger")
 proc=30
 semcor="data/semcor/semcor.data.xml"
@@ -10,5 +10,5 @@ test_words="data/semcor/semcor.eval.data.xml"
 cd ../
 for method in ${methods[*]}
 do
-    python interpretability_cli.py --test_words_weights "$test_weights" --test_words "$test_words" --embedding_path "$bert" -dense -numpy --lines_to_read -1 --smc_loader='semcor' --smc_path "$semcor" --processes $proc --distance "$method" --workspace $workspace --name "${method}_dense" -save --accuracy "accuracy@1";
+    python interpretability_cli.py --test_words_weights "$test_weights" --test_words "$test_words" --embedding_path "$bert" -dense -numpy --lines_to_read -1 --smc_loader='semcor' --smc_path "$semcor" --processes $proc --distance "$method" --workspace $workspace --name "${method}_dense" -load --accuracy "accuracy@1";
 done
