c9cbab92d84c580ee28004a517254aa649e07e61
Tamás Ficsor <ficstamas@gmail.com>
2021-03-09 09:31:46 +0100

    test file

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+semcor="/data/berend/WSD_Evaluation_Framework/Training_Corpora/SemCor/semcor.data.xml"
+all="/data/berend/WSD_Evaluation_Framework/Evaluation_Datasets/ALL/ALL.data.xml"
+
+train="/data/berend/representations_unreduced/sensebert-large-uncased/semcor.data.xml_sensebert-large-uncased_avg_False_layer_21-22-23-24.npy"
+test="/data/berend/representations_unreduced/sensebert-large-uncased/ALL.data.xml_sensebert-large-uncased_avg_False_layer_21-22-23-24.npy"
+workspace="/data/ficstamas/test/"
+config="/home/ficstamas/word_embedding_interpretability_dev/config/default.json"
+
+python src/preprocessing-cli.py --train "$train" --path "$workspace" --configuration "$config"
+python src/distance-cli.py --train "$train" --train_labels "$semcor" --label_processor "semcor-lexname" --path "$workspace" --distance "hellinger_normal" --jobs "30"
+python src/evaluation-cli.py --test "$test" --test_labels "$all" --label_processor "semcor-lexname" --path "$workspace" --evaluation_method "argmax" -label_frequency
